7cb01c3a67f97b9188634617d3709fdf653521b7
José Daniel Muñoz <mko.fcfm@hotmail.com>
2016-12-13 17:01:43 -0400

    Merge Listo

--- src/main/java/net/sourceforge/mochadoom/gamelogic/werewolf_t.java
+++ src/main/java/net/sourceforge/mochadoom/gamelogic/werewolf_t.java
@@ -7,26 +7,23 @@ public class werewolf_t extends monster_t {
 
 
 
+  @Override
+  public boolean isWerewolf() {
+    return true;
+  }
 
+  @Override
+  public float getSilverDamageMultiplier() {
+    return 2.0f;
+  }
 
+  @Override
+  public float getRegularDamageMultiplier() {
+    return super.getRegularDamageMultiplier();
+  }
 
-    @Override
-    public boolean isWerewolf(){
-        return true;
-    }
-    @Override
-    public float getSilverDamageMultiplier(){
-        return 2.0f;
-
-    }
-
-    @Override
-    public float getRegularDamageMultiplier() {
-        return super.getRegularDamageMultiplier();
-    }
-
-    @Override
-    public int getSpeedMult() {
-        return super.getSpeedMult()*getTimeAlive()/1000;
-    }
+  @Override
+  public int getSpeedMult() {
+    return super.getSpeedMult() * getTimeAlive() / 1000;
+  }
 }
