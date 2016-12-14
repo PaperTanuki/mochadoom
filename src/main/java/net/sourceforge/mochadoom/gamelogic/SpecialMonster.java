cd9afb92288d95ea0222148e774ea20e7c6f799b
diegodi <ddiaz3000@gmail.com>
2016-12-14 17:11:39 -0300

    changes to special monsters and leecher types

--- src/main/java/net/sourceforge/mochadoom/gamelogic/SpecialMonster.java
+++ src/main/java/net/sourceforge/mochadoom/gamelogic/SpecialMonster.java
@@ -2,6 +2,14 @@ package net.sourceforge.mochadoom.gamelogic;
 
 public class SpecialMonster extends monster_t{
 	
+	public SpecialMonster() {
+        super();
+    }
+    
+    public SpecialMonster(Actions A) {
+        super(A);
+    }
+    
     @Override
     public boolean canContaminate() {
       return true;
